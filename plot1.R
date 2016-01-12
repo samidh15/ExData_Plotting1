667eeb85e3bd6d35b5204fd88c3ad68f057c4c4c
Samidh <samidhc@gmail.com>
2016-01-11 22:22:48 -0500

    plot1 code and figure added

---
+++ plot1.R
@@ -0,0 +1,7 @@
+df <- read.csv('household_power_consumption.txt', sep = ';', header = TRUE)
+filtered_df <- df[df$Global_active_power != '?',]
+filtered_df_feb <- filtered_df[((filtered_df$Date == '1/2/2007') | (filtered_df$Date == '2/2/2007')),]
+png(file = 'plot1.R')
+hist(as.numeric(as.character(filtered_df_feb$Global_active_power)), xlab = 'Global Active Power (kilowatts)', col = 'red', main = 'Global Active Power', axes=F)
+axis(side = 2, at = c(seq(from=0,to=1200,by=200)))
+axis(side = 1, at = c(0,2,4,6))
